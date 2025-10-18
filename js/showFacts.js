f9468c00c6cf620b42b788972c096139030a00ee
David Schüppel <schueppeldavid@gmail.com>
2025-10-18 15:07:46 +0200

    chore: Reihenfolge der Länder geändert
    fix: Logik für factsCount korrigiert

--- js/showFacts.js
+++ js/showFacts.js
@@ -1,5 +1,5 @@
 let factsCount = 0;
-var countries = ["Japan","Spain","Puerto Rico","Estonia","Mali","Germany","France","Denmark","United States of America","Norway","Sweden","United Kingdom"];
+var countries = ["United Kingdom","Sweden","Norway","United States of America","France","Denmark","Germany","Estonia","Puerto Rico","Japan","Mali","Spain"];
 
 if (/Windows|Macintosh|Linux/.test(navigator.userAgent)) {
     validateCountryFacts(countries);
@@ -24,6 +24,7 @@ function validateCountryFacts(countries) {
     }
 
     // Gibt bei einem Test → Object.keys(facts) zurück.
+    factsCount--;
     const factCountries = showFact(countries[0], 0);
     const missingCountries = factCountries.filter(country => !countries.includes(country));
 
